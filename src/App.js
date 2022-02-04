60c5b763023766737f3924928972c02d7fd350ad
isaiah <isaiahmstockton@gmail.com>
2022-02-04 18:01:48 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [{
+        id: 1,
+        text: 'Practice Coding',
+        day: 'Feb 4th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Play Valorant',
+        day: 'Feb 4th at 5:30pm',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Check in on the homies',
+        day: 'Feb 4th at 7:30pm',
+        reminder: true,
+    },
+  ])
+
+  // Add Task
+  // 1:09:23
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks Left!'}
     </div>
   );
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+  return ( 
+    <form className='add-form'>
+      <div className='form-control'>
+          <label>Task</label>
+          <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)}></input>
+        </div>
+        <div className='form-control'>
+          <label>Day & Time</label>
+          <input type='text' placeholder='Add Day & Time' value={day} onChange={(e) => setDay(e.target.value)}></input>
+        </div>
+        <div className='form-control form-control-check' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}>
+          <label>Set Reminder</label>
+          <input type='checkbox' ></input>
+        </div>
+
+        <input type='submit' value='Save Task' className='btn btn-block'></input>
+    </form>
+  )
+}
+
+export default AddTask;
